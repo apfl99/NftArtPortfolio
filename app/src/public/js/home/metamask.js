fb9ac96c32cfdf2049083646b8f44f563a7d24ed
chlwotjd66 <choi_jasung@naver.com>
2022-04-01 14:52:47 +0900

    no message

--- app/src/public/js/home/metamask.js
+++ app/src/public/js/home/metamask.js
@@ -1,12 +1,7 @@
 const ethereumButton = document.querySelector('.enableEthereumButton');
-const showAccount = document.querySelector('.showAccount');
 
 ethereumButton.addEventListener('click', () => {
-  getAccount();
-});
+  //Will Start the metamask extension
+  ethereum.request({ method: 'eth_requestAccounts' });
 
-async function getAccount() {
-  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-  const account = accounts[0];
-  showAccount.innerHTML = account;
-}
+});
