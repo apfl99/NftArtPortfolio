bc145a4a043b04b70a840ea83175d386d2c2ef7d
chlwotjd66 <choi_jasung@naver.com>
2022-04-03 19:55:27 +0900

    no message

--- app/src/public/js/home/metamask.js
+++ app/src/public/js/home/metamask.js
@@ -1,7 +1,56 @@
-const ethereumButton = document.querySelector('.enableEthereumButton');
 
-ethereumButton.addEventListener('click', () => {
-  //Will Start the metamask extension
-  ethereum.request({ method: 'eth_requestAccounts' });
-
-});
+                      window.userWalletAddress = null
+                      const loginButton = document.getElementById('#loginButton')
+                      const userWallet = document.getElementById('#userWallet')
+                  
+                      // $(function(){
+                      //   $("#loginButton").click(function(){
+                      //     //사용하고자 하는 함수ß
+                      //     fn_kakaoLogin('Kakao');
+                          
+                      //   });
+                      // });	
+                      
+                      function toggleButton() {
+                        if (!window.ethereum) {
+                          loginButton.innerText = 'MetaMask is not installed'
+                          loginButton.classList.remove('bg-purple-500', 'text-white')
+                          loginButton.classList.add('bg-gray-500', 'text-gray-100', 'cursor-not-allowed')
+                          return false
+                        }
+                  
+                        loginButton.addEventListener('click', loginWithMetaMask)
+                      }
+                  
+                      async function loginWithMetaMask() {
+                        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+                          .catch((e) => {
+                            console.error(e.message)
+                            return
+                          })
+                        if (!accounts) { return }
+                  
+                        window.userWalletAddress = accounts[0]
+                        userWallet.innerText = window.userWalletAddress
+                        loginButton.innerText = 'Sign out of MetaMask'
+                  
+                        loginButton.removeEventListener('click', loginWithMetaMask)
+                        setTimeout(() => {
+                          loginButton.addEventListener('click', signOutOfMetaMask)
+                        }, 200)
+                      }
+                  
+                      function signOutOfMetaMask() {
+                        window.userWalletAddress = null
+                        userWallet.innerText = ''
+                        loginButton.innerText = 'Sign in with MetaMask'
+                  
+                        loginButton.removeEventListener('click', signOutOfMetaMask)
+                        setTimeout(() => {
+                          loginButton.addEventListener('click', loginWithMetaMask)
+                        }, 200)
+                      }
+                  
+                      window.addEventListener('DOMContentLoaded', () => {
+                        toggleButton()
+                      });
