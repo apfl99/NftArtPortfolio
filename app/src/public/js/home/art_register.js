3641fa592c3f6cb6da9c3e23d5b6f903f8342164
apfl99 <rmrehd604@gmail.com>
2022-05-24 18:20:36 +0900

    파일 업로드 확장자 및 용량 처리

--- app/src/public/js/home/art_register.js
+++ app/src/public/js/home/art_register.js
@@ -13,32 +13,87 @@ function art_register(e) {
         const artCategory = document.getElementById('artCategory');
         const artDescription = document.getElementById('artDescription');
 
-        //폼 데이터 처리
-        const formData = new FormData();
 
-        
-        formData.append("artName",artName.value);
-        formData.append("artCategory",artCategory.value);
-        formData.append("artDescription",artDescription.value);
-        formData.append("files", files.files[0]);
-        formData.append("userId",window.sessionStorage.getItem('userId'));
-        
-        //프론트 -> 서버(form 형식)
-        fetch("/artRegister", {
-                method: 'POST',
-                body: formData,
-        })
-        .then((res) => res.json())
-        .then((res) => {
-            if (res.success) {
-                alert('작품 업로드 성공');
-                location.href='/mypage';
-            } else {
-                alert(res.msg);
-            }
-        })
-        .catch((err) => {
-            console.error("작품 업로드 중 에러가 발생하였습니다.");
-        });
+        // 미입력 체크
+        if(checkValue(files.files[0])){
+            $('#files-message').text('');
+        } else {
+            $('#files-message').text('파일을 업로드해주세요.');
+        }
+        if(checkValue(artName.value)){
+            $('#artName-message').text('');
+        } else {
+            $('#artName-message').text('작품명을 입력해주세요.');
+        }
+        if(checkValue(artDescription.value)) {
+            $('#artDescription-message').text('');
+        } else {
+            $('#artDescription-message').text('작품 설명을 입력해주세요.');
+        }
+
+        const checkResult = checkValue(files.files[0]) && checkValue(artName.value) && checkValue(artDescription.value) && fileCheck_size(files.files[0]) && fileCheck_ext();
+
+        if(checkResult) {
+            //폼 데이터 처리
+            const formData = new FormData();
+
+            
+            formData.append("artName",artName.value);
+            formData.append("artCategory",artCategory.value);
+            formData.append("artDescription",artDescription.value);
+            formData.append("files", files.files[0]);
+            formData.append("userId",window.sessionStorage.getItem('userId'));
+            
+            //프론트 -> 서버(form 형식)
+            fetch("/artRegister", {
+                    method: 'POST',
+                    body: formData,
+            })
+            .then((res) => res.json())
+            .then((res) => {
+                if (res.success) {
+                    alert('작품 업로드 성공');
+                    location.href='/mypage';
+                } else {
+                    alert(res.msg);
+                }
+            })
+            .catch((err) => {
+                console.error("작품 업로드 중 에러가 발생하였습니다.");
+            });
+        }
+
 };
   
+function checkValue(value) {
+    var valueCheck = value;
+    if( !valueCheck ){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+//파일 용량 체크
+function fileCheck_size(file) {
+
+    var maxSize = 5 * 1024 * 1024;
+    if(file.size > maxSize){
+        alert('파일 용량이 너무 큽니다.');
+        return false;
+    } else {
+        return true;
+    }
+}
+
+//파일 확장자 체크
+function fileCheck_ext() {
+    
+    var ext = $("input[name='files']").val().split('.').pop().toLowerCase();
+    if($.inArray(ext, ['gif','png','jpg','jpeg','svg']) == -1) {
+        alert("등록 할수 없는 확장자입니다.");
+        return false;
+    } else {
+        return true;
+    }
+}
